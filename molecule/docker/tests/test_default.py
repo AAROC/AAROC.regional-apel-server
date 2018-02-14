e95e9a78352d83962a502f59a0833a4cc2ea10a2
Bruce Becker <brucellino@gmail.com>
2018-02-14 10:46:06 +0100

    Fix python linting errors

--- molecule/docker/tests/test_default.py
+++ molecule/docker/tests/test_default.py
@@ -13,14 +13,16 @@ def test_hosts_file(host):
     assert f.user == 'root'
     assert f.group == 'root'
 
+
 def test_config_files(host):
     apel_config_dir = host.file('/etc/apel')
-    
+
     # Various apel config files
     apel_auth_config_file = host.file('/etc/apel/auth.cfg')
-    apel_db_config_file   = host.file('/etc/apel/db.cfg')
-    apel_receiver_file    = host.file('/etc/apel/receiver.cfg')
-    
+    apel_db_config_file = host.file('/etc/apel/db.cfg')
+    apel_receiver_file = host.file('/etc/apel/receiver.cfg')
+
+    assert apel_config_dir.is_directory
     assert apel_auth_config_file.exists
     assert apel_db_config_file.exists
     assert apel_receiver_file.exists
